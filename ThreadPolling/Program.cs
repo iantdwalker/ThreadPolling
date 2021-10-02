0d2774252400cfa74d13e1c36e329fcf5f2b79e5
Ian <iantdwalker@gmail.com>
2021-10-02 11:04:14 +0100

    Sample project to demonstrate how to do asyncronous thread polling with cancellations using tasks

---
+++ ThreadPolling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ThreadPolling
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var progress = new Progress<string>();
+			progress.ProgressChanged += Progress_ProgressChanged;
+			var taskRunner = new TaskRunner(progress);
+			Console.WriteLine("Starting a TaskRunner...press any key to cancel.");
+			
+			await taskRunner.StartAsync();
+			
+			Console.ReadKey();
+			taskRunner.Cancel();
+			Console.WriteLine("TaskRunner was cancelled.");
+		}
+
+		private static void Progress_ProgressChanged(object sender, string e)
+		{
+			Console.WriteLine(e);
+		}
+	}
+}
---
+++ ThreadPolling/TaskRunner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPolling
+{
+	public class TaskRunner
+	{
+		private IProgress<string> _progress;
+		private CancellationTokenSource _cancellationTokenSource;
+		private int _taskPollingInterval = 5000;
+ 		
+		public TaskRunner(IProgress<string> progress)
+		{
+			_progress = progress;
+			_cancellationTokenSource = new CancellationTokenSource();
+		}
+
+		public async Task StartAsync()
+		{
+			await Task.Factory.StartNew(ProcessAsync, _cancellationTokenSource.Token);
+		}
+
+		public void Cancel()
+		{
+			_cancellationTokenSource.Cancel();
+		}
+
+		private async Task ProcessAsync(object taskState)
+		{
+			var token = (CancellationToken)taskState;
+			var stopWatch = new Stopwatch();
+			stopWatch.Start();
+
+			while (!token.IsCancellationRequested)
+			{
+				_progress.Report($"{stopWatch.ElapsedMilliseconds / 1000} seconds have elapsed.");
+
+				// Passing token here allows the Delay to be cancelled if your task gets cancelled.
+				await Task.Delay(_taskPollingInterval, token);
+			}
+
+			token.ThrowIfCancellationRequested();
+		}
+	}
+}
